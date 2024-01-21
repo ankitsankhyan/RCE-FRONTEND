765eba24f81639d24ebb2732518ed76e96e85fcb
vivekup3424 <vivek.hygfrt3424@gmail.com>
2024-01-21 17:10:51 +0530

    added fetch functionality for output and input

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import client from "./client";
 import { MdOutlineSaveAlt } from "react-icons/md";
 import { FaPlay } from "react-icons/fa6";
 import { FaRegFileCode } from "react-icons/fa";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
 const body = document.getElementsByTagName("body");
+
 function download(filename, text) {
   var element = document.createElement("a");
   element.setAttribute(
@@ -18,19 +21,16 @@ function download(filename, text) {
   document.body.appendChild(element);
   element.click();
   document.body.removeChild(element);
-
-
 }
 
 function App() {
+  //state variable needed to fetch input and output
+  const [runButtonPressed, setRunButtonPressed] = useState(false);
+
   const [input, updateInput] = useState("");
-  const [value, updateValue] = useState(`#include <stdio.h>
-  int main() {
-      // Write C code here
-      printf("Hello world");
-  return 0;
-  }`);
-  const [language, updateLanguage] = useState("C");
+  const [output, updateOutput] = useState("");
+  const [value, updateValue] = useState(`print("Hello World")`);
+  const [language, updateLanguage] = useState("python");
   const [jsvalue, updatejsValue] = useState(`
   console.log("Welcome to Online Program");`);
 
@@ -39,7 +39,6 @@ function App() {
           System.out.println("Hello, World!");
       }
   }`);
-
 
   const [pythonValue, updatepythonValue] = useState(`print("Hello world")`);
   const [cppValue, updatecppValue] = useState(`#include <iostream>
@@ -54,16 +53,66 @@ function App() {
       printf("Hello world");
   return 0;
   }`);
-  
-
-  const [preview, updatePreview] = useState("");
   const [dark, updateDark] = useState(false);
-  const [selected, updateSelected] = useState("C");
+  const [selected, updateSelected] = useState("python");
+  const handleSave = () => {
+    download(
+      "download.txt",
+      selected === "JS" ? jsvalue : selected === "C++" ? cppValue : selected === "java" ? javaValue : selected === "C" ? Cvalue : pythonValue
+    );
+  };
+
+  const handleRun = () => {
+    let data = { value, language, input };
+    setRunButtonPressed(true); //important step to make the fetch the input and output
+    client.post("/api/code", data)
+      .then(response => {
+        console.log(response);
+        updateOutput(response.data.output);
+      })
+      .catch(error => {
+        console.error(error);
+        toast.error("Error running code. Please check console for details.");
+      });
+  };
+
+  //fetch input.txt and output.txt content after making the API request,
+  useEffect(() => {
+    if(runButtonPressed){
+      // Fetch input.txt content
+    axios.get("/api/input")
+      .then(response => {
+        updateInput(response.data.content);
+      })
+      .catch(error => {
+        console.error(error);
+        toast.error("Error fetching input.txt content. Please check console for details.");
+      });
+
+    // Fetch output.txt content
+    axios.get("/api/output")
+      .then(response => {
+        updateOutput(response.data.content);
+      })
+      .catch(error => {
+        console.error(error);
+        toast.error("Error fetching output.txt content. Please check console for details.");
+      });
+  }}, [runButtonPressed]);
 
   return (
     <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss={true}
+        draggable={true}
+        pauseOnHover={true}
+      />
       <div className="navbar">
-      
         <div
           title={dark ? "go light" : "go dark"}
           className={`material-icons ${dark ? "colorWhite" : ""}`}
@@ -72,36 +121,26 @@ function App() {
             updateDark(!dark);
           }}
         >
-           <FaRegFileCode />
-           Pro Editor
-
+          <FaRegFileCode />
+          Pro Editor
         </div>
-      <div className="options">
-      <button
-          title="save"
-          className={`save ${dark ? "darkRun" : "colorWhite"}`}
-          onClick={() => {
-            download(
-              "download.txt",
-              selected === "JS" ? jsvalue : selected === "C++" ? cppValue : selected === "java" ? javaValue : selected === "python" ? pythonValue : Cvalue
-            );
-          }}
-        >
-          <MdOutlineSaveAlt style={{transform:"scale(1.5)"}}  />
-        </button>
-        <button
-          className={`save ${dark ? "darkRun" : "colorWhite"}`}
-          onClick={() => {
-            
-             let data = {value, language,input};
-             console.log(data);
-              client.post("/run", data);
-          }}
-        >
-        <FaPlay style={{transform:"scale(1.5)"}} />
-        </button>
-      </div>
-       
+        <div className="options">
+          <button
+            title="save"
+            className={`save ${dark ? "darkRun" : "colorWhite"}`}
+            onClick={handleSave}
+          >
+            <MdOutlineSaveAlt style={{ transform: "scale(1.5)" }} />
+          </button>
+          <button
+            className={`save ${dark ? "darkRun" : "colorWhite"}`}
+            onClick={handleRun}
+            id = "runButton"
+            name = "run"
+          >
+            <FaPlay style={{ transform: "scale(1.5)" }} />
+          </button>
+        </div>
       </div>
       <div className="playground">
         <select
@@ -118,17 +157,14 @@ function App() {
           <option value="python">Python</option>
         </select>
         <div className={`editor1 mr-0 ${dark ? "colorDark" : ""}`}>
-          
- 
-    
-   {selected === "C" && (
+          {selected === "C" && (
             <Editor
               mode="c_cpp"
               dark={dark}
               onChange={(e) => {
                 updateCValue(e);
                 updateValue(e);
-                updateLanguage("C")
+                updateLanguage("C");
               }}
               value={Cvalue}
             />
@@ -156,60 +192,46 @@ function App() {
               }}
               value={cppValue}
             />
-
           )}
-          {selected === 'java' &&(
-           <Editor
-           mode="java"
-           dark={dark}
-           onChange={(e) => {
-             updatejavaValue(e);
-             updateValue(e);
-             updateLanguage("java");
-           }}
-           value={javaValue}
-         />
+          {selected === 'java' && (
+            <Editor
+              mode="java"
+              dark={dark}
+              onChange={(e) => {
+                updatejavaValue(e);
+                updateValue(e);
+                updateLanguage("java");
+              }}
+              value={javaValue}
+            />
           )}
-
-      {selected === 'python' &&(
-           <Editor
-           mode="python"
-           dark={dark}
-           onChange={(e) => {
-             updatepythonValue(e);
-             updateValue(e);
-             updateLanguage("python");
-           }}
-           value={pythonValue}
-         />
+          {selected === 'python' && (
+            <Editor
+              mode="python"
+              dark={dark}
+              onChange={(e) => {
+                updatepythonValue(e);
+                updateValue(e);
+                updateLanguage("python");
+              }}
+              value={pythonValue}
+            />
           )}
-          
-
-  </div>
-          
-  <div className={`editor ${dark ? "colorDark" : ""}`}>
-    <div className="extra">
-    
-    <h4 style={{margin:"0px"}}>Input</h4>
-    <div className="input">
-          
-          <textarea type="text" onChange={(e)=>{updateInput(e.target.value)}}/>
         </div>
-        <h4 style={{marginTop:"50px", marginBottom:"0px"}}>Output</h4>
-         <div className="output">
-         
-         </div>
-    </div>
-        
-        
+        <div className={`editor ${dark ? "colorDark" : ""}`}>
+          <div className="extra">
+            <h4 style={{ margin: "0px" }}>Input</h4>
+            <div className="input">
+              <textarea type="text" onChange={(e) => { updateInput(e.target.value) }} value={input} />
+            </div>
+            <h4 style={{ marginTop: "50px", marginBottom: "0px" }}>Output</h4>
+            <div className="output">
+              <div>
+                {output}
+              </div>
+            </div>
+          </div>
         </div>
-       
-       
-        
-      
-       
-          
-      
       </div>
     </div>
   );
